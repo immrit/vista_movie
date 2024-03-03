1faafd2e805516060206f29fc6fc4bdf897d577f
ahmadesmaili <38140607+immrit@users.noreply.github.com>
2024-03-03 10:43:00 +0330

    lat

--- lib/Di/fetchData.dart
+++ lib/Di/fetchData.dart
@@ -1,53 +1,55 @@
 import 'package:dio/dio.dart';
 import '../Models/DataModel.dart';
-  class DataFetcher {
-    final String cName;
-    final String gName;
-    late final String url;
-    BaseOptions options = new BaseOptions(
-        connectTimeout: Duration(milliseconds: 20000),
-        receiveTimeout: Duration(milliseconds: 20000)
-    );
-    // final List<String> categoryUrls = [
-    //   'https://vista.chbk.run/api/collections/Movies/records',
-    //   'https://vista.chbk.run/api/collections/Serials/records',
-    //   'https://vista.chbk.run/api/collections/Slider/records',
-    //   'https://vista.chbk.run/api/collections/categories/records',
-    // ];
-    // List<Map<String, dynamic>> moviesDataList = [];
-    // List<Map<String, dynamic>> seriesDataList = [];
-    // List<Map<String, dynamic>> slidersDataList = [];
-    // List<Map<String, dynamic>> categoriesDataList = [];
-    static List<DataModel> seriesGenerAction = [];
-    static List<DataModel> seriesGenerDrama = [];
-    static List<DataModel> seriesGenerFantasy = [];
-    static List<DataModel> moviesGenerAction = [];
-    static List<DataModel> moviesGenerDrama = [];
-    static List<DataModel> moviesGenerFantasy = [];
-    DataFetcher({
-      required this.cName,
-      required this.gName,
-    }
-    ) {
-      url = 'https://vista.chbk.run/api/collections/$cName/records';
-    }
-    Future<List<DataModel>> fetchGenre() async {
+
+class DataFetcher {
+  final String cName;
+  final String gName;
+  late final String url;
+  BaseOptions options = new BaseOptions(
+      connectTimeout: Duration(milliseconds: 20000),
+      receiveTimeout: Duration(milliseconds: 20000));
+  // final List<String> categoryUrls = [
+  //   'https://vista.chbk.run/api/collections/Movies/records',
+  //   'https://vista.chbk.run/api/collections/Serials/records',
+  //   'https://vista.chbk.run/api/collections/Slider/records',
+  //   'https://vista.chbk.run/api/collections/categories/records',
+  // ];
+  // List<Map<String, dynamic>> moviesDataList = [];
+  // List<Map<String, dynamic>> seriesDataList = [];
+  // List<Map<String, dynamic>> slidersDataList = [];
+  // List<Map<String, dynamic>> categoriesDataList = [];
+  // static List<DataModel> seriesGenerAction = [];
+  // static List<DataModel> seriesGenerDrama = [];
+  // static List<DataModel> seriesGenerFantasy = [];
+  // static List<DataModel> moviesGenerAction = [];
+  // static List<DataModel> moviesGenerDrama = [];
+  // static List<DataModel> moviesGenerFantasy = [];
+  DataFetcher({
+    required this.cName,
+    required this.gName,
+  }) {
+    url = 'https://vista.chbk.run/api/collections/$cName/records';
+  }
+  Future<List<DataModel>> fetchGenre() async {
     while (true) {
       await Future.delayed(Duration(seconds: 3));
       try {
         BaseOptions options = new BaseOptions(
             connectTimeout: Duration(milliseconds: 20000),
-            receiveTimeout: Duration(milliseconds: 20000)
-        );
+            receiveTimeout: Duration(milliseconds: 20000));
         Dio dio = new Dio(options);
         // dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: false, requestHeader: true));
         final response = await dio.get(
           url,
-          queryParameters: {'expand': 'genre','filter': 'genre ~ "$gName"',},
+          queryParameters: {
+            'expand': 'genre',
+            'filter': 'genre ~ "$gName"',
+          },
         );
         if (response.statusCode == 200) {
-          List<dynamic> jsonItems = response.data['items'] as  List ;
-          List<DataModel> dataList = jsonItems.map((item) => DataModel.fromMapJson(item)).toList();
+          List<dynamic> jsonItems = response.data['items'] as List;
+          List<DataModel> dataList =
+              jsonItems.map((item) => DataModel.fromMapJson(item)).toList();
           // saveGenerData(dataList);
           return dataList;
         }
@@ -56,32 +58,32 @@ import '../Models/DataModel.dart';
       }
       return [];
     }
-    }
-    saveGenerData(List<dynamic> data) async {
-      if (cName == 'series') {
-        switch (gName) {
-          case 'drama':
-            seriesGenerDrama = List<DataModel>.from(data);
-            break;
-          case 'action':
-            seriesGenerAction = List<DataModel>.from(data);
-            break;
-          case 'fantasy':
-            seriesGenerFantasy = List<DataModel>.from(data);
-            break;
-        }
-      } else if (cName == 'movies') {
-        switch (gName) {
-          case 'drama':
-            moviesGenerDrama = List<DataModel>.from(data);
-            break;
-          case 'action':
-            moviesGenerAction = List<DataModel>.from(data);
-            break;
-          case 'fantasy':
-            moviesGenerFantasy = List<DataModel>.from(data);
-            break;
-        }
-      }
-    }
   }
+  // saveGenerData(List<dynamic> data) async {
+  //   if (cName == 'series') {
+  //     switch (gName) {
+  //       case 'drama':
+  //         seriesGenerDrama = List<DataModel>.from(data);
+  //         break;
+  //       case 'action':
+  //         seriesGenerAction = List<DataModel>.from(data);
+  //         break;
+  //       case 'fantasy':
+  //         seriesGenerFantasy = List<DataModel>.from(data);
+  //         break;
+  //     }
+  //   } else if (cName == 'movies') {
+  //     switch (gName) {
+  //       case 'drama':
+  //         moviesGenerDrama = List<DataModel>.from(data);
+  //         break;
+  //       case 'action':
+  //         moviesGenerAction = List<DataModel>.from(data);
+  //         break;
+  //       case 'fantasy':
+  //         moviesGenerFantasy = List<DataModel>.from(data);
+  //         break;
+  //     }
+  //   }
+  // }
+}
--- lib/Models/DataModel.dart
+++ lib/Models/DataModel.dart
@@ -86,23 +86,24 @@ class DataModel {
   final String url;
   final String subTitle;
   final List<String> genre;
-  DataModel(this.id,this.name,this.logo,this.collectionId,this.url,this.subTitle,this.genre);
-  factory DataModel.fromMapJson(Map<String,dynamic> jsonObject){
+  DataModel(this.id, this.name, this.logo, this.collectionId, this.url,
+      this.subTitle, this.genre);
+  factory DataModel.fromMapJson(Map<String, dynamic> jsonObject) {
     List<String> genre = [];
+
     if (jsonObject['expand'] != null && jsonObject['expand']['genre'] != null) {
       jsonObject['expand']['genre'].forEach((genreItem) {
         genre.add(genreItem['genre']);
       });
     }
     return DataModel(
-      jsonObject['id'],
-      jsonObject['name'],
-      jsonObject['logo'],
-      jsonObject['collectionId'],
-      jsonObject['url'],
-      jsonObject['subtitle'],
-      genre
-    );
+        jsonObject['id'],
+        jsonObject['name'],
+        jsonObject['logo'],
+        jsonObject['collectionId'],
+        jsonObject['url'],
+        jsonObject['subtitle'],
+        genre);
   }
 // DataModel({
 //   required this.id,
